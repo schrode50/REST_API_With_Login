c0b0759927ff662bdc68b0b9f853ea2ea645f902
Stephen Schroeder <schrode50@gmail.com>
2016-06-02 10:01:21 -0700

    fixed my stupidity

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -8,24 +8,26 @@ const basicHTTP = require(__dirname + '/../lib/basic_http');
 const router = module.exports = exports = express.Router();
 
 router.post('/signup', bodyParser, (req, res, next) => {
+  console.log(req.body, 'little string');
   let newUser = new User(req.body);
-  let hashedPassword = newUser.hashedPassword();
+  let hashedPassword = newUser.hashPassword();
   newUser.password = hashedPassword;
   req.body.password = null;
   User.findOne({ username: req.body.username }, (err, user) => {
     if(err || user) return next(new Error('CANNOT CREATE USER'));
     newUser.save((err, user) => {
       if(err) return next(new Error('Cannot Create User'));
-      res.json({ token: user.generateToken() });
+      res.json({ token: 'you made it in' });
     });
   });
 });
 
 router.get('/signin', basicHTTP, (req, res, next) => {
+  console.log(res.body, 'big string');
   User.findOne({ username: req.auth.username }, (err, user) => {
     if(!user || err) return next(new Error('CANNOT SIGN IN'));
     if(!user.comparePassword(req.auth.password)) return next(new Error('CANNOT SIGN IN'));
 
-    res.json({ token: user.generateToken() });
+    res.json({ token: 'you made it in' });
   });
 });
